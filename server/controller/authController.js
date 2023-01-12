f33b1895ef4c60f66f81c8c5a5b60a679a9140ab
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-13 04:39:53 +0530

    feature: auth init

---
+++ server/controller/authController.js
@@ -0,0 +1,8 @@
+const adminEmail = process.env.ADMIN_EMAIL;
+const adminPassword = process.env.ADMIN_PASSWORD;
+
+exports.login = async (req, res) => {};
+
+exports.refreshToken = async (req, res) => {};
+
+exports.logout = async (req, res) => {};
