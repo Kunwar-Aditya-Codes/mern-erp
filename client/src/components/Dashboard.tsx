ea74d36d987434c189258599da22a32d159edc41
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-19 11:47:20 +0530

    added: side nav for responsive

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,20 +1,25 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { Bars3Icon } from '@heroicons/react/24/solid';
 
 const Dashboard = () => {
-  const [sidebar, setSidebar] = useState(true);
+  const [sidebar, setSidebar] = useState<Boolean>(false);
 
   return (
     <div className='relative flex h-full'>
       <div
         className={`${
-          sidebar ? 'tanslate-x-0' : '-translate-x-full'
-        } absolute h-full w-full transform border-b-2 border-zinc-300/20 bg-zinc-900/80 duration-300 ease-in-out md:relative md:flex-[0.2]`}
+          sidebar ? 'translate-x-0' : '-translate-x-full'
+        } absolute h-full w-full transform border-b-2 border-zinc-300/20 bg-zinc-900/80 duration-300 ease-in-out md:relative md:flex-[0.2]  md:translate-x-0`}
       >
-        <Sidebar />
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
       </div>
-      <div className='md:flex-[0.8]'>
+      <div className='space-y-4 p-4 md:flex-[0.8]'>
+        <Bars3Icon
+          className='h-5 w-5 cursor-pointer md:hidden'
+          onClick={() => setSidebar(!sidebar)}
+        />
         <Outlet />
       </div>
     </div>
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,9 +1,31 @@
-const Sidebar = () => {
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+const Sidebar = ({
+  sidebar,
+  setSidebar,
+}: {
+  sidebar: Boolean;
+  setSidebar: React.Dispatch<React.SetStateAction<Boolean>>;
+}) => {
   return (
-    <div className='p-4'>
-      <h1 className='rounded-md bg-violet-700 p-2 text-white'>
+    <div className='flex h-full flex-col p-4'>
+      <XMarkIcon
+        className='mb-4 h-5 w-5 cursor-pointer md:hidden'
+        onClick={() => setSidebar(!sidebar)}
+      />
+
+      <h1 className='rounded-md border-2 border-violet-700 p-2 text-zinc-300'>
         Admin Dashboard
       </h1>
+
+      <div className='mt-6 flex-grow space-y-4 border-t-2 border-zinc-300/20 p-2  tracking-wide  text-zinc-500 underline underline-offset-[5px] md:text-lg'>
+        <h1 className='mt-4 cursor-pointer rounded-md p-3 hover:bg-zinc-300/10'>
+          View Students
+        </h1>
+        <h1 className='cursor-pointer rounded-md p-3 hover:bg-zinc-300/10'>
+          View Teacher
+        </h1>
+      </div>
     </div>
   );
 };
