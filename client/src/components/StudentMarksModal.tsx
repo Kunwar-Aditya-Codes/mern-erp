6563af551b0463f048c7e4d7d89e807ec97e3043
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-02-03 07:18:25 +0530

    init: marks modal

---
+++ client/src/components/StudentMarksModal.tsx
@@ -0,0 +1,10 @@
+const StudentMarksModal = ({ setModal, studentId }: any) => {
+  console.log();
+
+  return (
+    <div className='absolute left-0  right-0 bottom-0 h-full  bg-black/90 mb-4'>
+      StudentMarksModal
+    </div>
+  );
+};
+export default StudentMarksModal;
--- client/src/pages/Teacher/StudentMarks.tsx
+++ client/src/pages/Teacher/StudentMarks.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useGetStudentMarksByIdQuery } from '../../app/slices/teacherApiSlice';
+import StudentMarksModal from '../../components/StudentMarksModal';
 
 const StudentMarks = () => {
+  const [modal, setModal] = useState(false);
+
   const { id } = useParams();
 
   const { data, isLoading, error } = useGetStudentMarksByIdQuery(id);
@@ -12,8 +16,8 @@ const StudentMarks = () => {
     <div>Some error occured...</div>
   ) : (
     data && (
-      <div className='mx-auto max-w-4xl space-y-16'>
-        <div className='flex items-center justify-between'>
+      <div className=' mx-auto h-full max-w-4xl  space-y-16'>
+        <div className=' flex  items-center justify-between'>
           <h1 className='text-2xl font-bold tracking-wide lg:text-6xl '>
             {data.marks.studentId.sName}
           </h1>
@@ -24,7 +28,7 @@ const StudentMarks = () => {
           />
         </div>
 
-        <div className='lg:text-2xl'>
+        <div className=' lg:text-2xl'>
           {data.marks.subjectMarks.length !== 0 ? (
             data.marks.subjectMarks.map((sub: any) => (
               <div key={sub._id} className='my-2 flex space-x-2'>
@@ -38,9 +42,12 @@ const StudentMarks = () => {
           )}
         </div>
 
-        <div className='flex items-center justify-start space-x-24 lg:text-2xl'>
-          <button className='rounded-md bg-violet-600 px-4 py-2 text-white'>
-            Add Marks
+        <div className=' flex  items-center justify-start space-x-24 '>
+          <button
+            onClick={() => setModal(true)}
+            className='rounded-md bg-violet-600 px-4 py-2 text-white'
+          >
+            Add/Update Marks
           </button>
 
           <Link to='/dashboard/teacher/view-students'>
@@ -49,6 +56,8 @@ const StudentMarks = () => {
             </button>
           </Link>
         </div>
+
+        {modal && <StudentMarksModal setModal={setModal} studentId={id} />}
       </div>
     )
   );
